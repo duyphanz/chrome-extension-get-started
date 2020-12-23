47cff797ee49e3d240f0f13fbf44489f326ed743
Duy Phan <duyphan@lecle.co.kr>
2020-12-23 09:17:27 +0700

    feat: added breadcrumbs

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 chrome.browserAction.onClicked.addListener(buttonClicked);
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  
+  chrome.bookmarks.getChildren(msg.id, function (result){
+    sendResponse(result);
+  })
+  return true;
+});
 
 function buttonClicked(tab) {
-  chrome.tabs.sendMessage(tab.id, 'ACTIVE')
+  chrome.bookmarks.getTree(function (results) {
+    const message = {
+      key: 'ACTIVE',
+      payload: results
+    }
+    chrome.tabs.sendMessage(tab.id, message)
+  })
 }
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 
+let bookMarkTree = null;
+let breadcrumbs = ['root'];
+
 function gotMessage(message, sender, sendResponse) {
-  if(message === 'ACTIVE') {
+  bookMarkTree = message.payload;
+  if(message.key === 'ACTIVE') {
     createDialog()
   }
 }
@@ -12,8 +16,34 @@ function createDialog() {
   if(dialog) {
     dialog.classList.toggle('active')
   } else {
-    const dialog = document.createElement('DIV');
+    dialog = document.createElement('DIV');
     dialog.setAttribute('class', 'chrome-ex-dialog active');
+
+    const breadcrumbsEl = document.createElement('P');
+    const content = document.createElement('DIV');
+    content.setAttribute('class', 'chrome-ex-content');
+
+    dialog.appendChild(breadcrumbsEl)
+    dialog.appendChild(content)
+
+    createBookmarkItem(bookMarkTree[0].children, content, breadcrumbsEl)
     document.body.appendChild(dialog);
+  }
+}
+
+function createBookmarkItem(items, content, breadcrumbsEl) {
+  for(let item of items) {
+    const button = document.createElement('BUTTON');
+    button.setAttribute('class', 'bookmark-item');
+    button.onclick = function() {
+      content.textContent = '';
+      breadcrumbs.push(item.title)
+      breadcrumbsEl.innerText = breadcrumbs.join(' > ')
+      chrome.runtime.sendMessage({ id: item.id }, function(response) {
+        createBookmarkItem(response, content, breadcrumbsEl)
+      });
+    }
+    button.innerText = item.title;
+    content.appendChild(button)
   }
 }
