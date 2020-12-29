71c0498bd5f38b219dea5910301e38f2dcbf0cf6
Duy Phan <duyphan@lecle.co.kr>
2020-12-29 16:40:41 +0700

    feat: show created labels

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,105 +2,126 @@
 const historyDir = [];
 let isLabelTab = false;
 
-const bmTree = document.querySelector('.bookmark-tree')
-const backButton = document.querySelector('.go-back-btn')
-const createLabelButton = document.querySelector('.create-lable-btn')
-const itemList = document.querySelector('.bookmark-items')
-const labelContainer = document.querySelector('.label-container')
-const bookmarkContainer = document.querySelector('.bookmark-container')
+const bmTree = document.querySelector(".bookmark-tree");
+const backButton = document.querySelector(".go-back-btn");
+const createLabelButton = document.querySelector(".create-lable-btn");
+const itemList = document.querySelector(".bookmark-items");
+const labelContainer = document.querySelector(".label-container");
+const bookmarkContainer = document.querySelector(".bookmark-container");
+const colorsContainer = document.querySelector(".colors-container");
 
 // add Eventlistener
-backButton.addEventListener('click', onBack)
-createLabelButton.addEventListener('click', onMoveToLabel)
+backButton.addEventListener("click", onBack);
+createLabelButton.addEventListener("click", onMoveToLabel);
 
 // init DOMTree state
-labelContainer.setAttribute('class', 'hidden')
+labelContainer.setAttribute("class", "hidden");
 
 // get init bookmark tree
+chrome.storage.sync.set({
+  colors: [
+    { l: "abc", c: "black" },
+    { l: "abc", c: "red" },
+    { l: "abc", c: "green" },
+  ],
+});
+
 chrome.bookmarks.getTree(function (results) {
-  createBookmarkTree(results[0].children)
-})
+  createBookmarkTree(results[0].children);
+});
 
 function createBookmarkTree(items) {
-  bmTree.textContent = '';
+  bmTree.textContent = "";
 
-  for(let item of items) {
-    const button = document.createElement('button')
+  for (let item of items) {
+    const button = document.createElement("button");
 
-    button.setAttribute('class', 'bookmark-item')
-    button.onclick = function() {
+    button.setAttribute("class", "bookmark-item");
+    button.onclick = function () {
       backButton.innerText = item.title;
       historyDir.push(item);
 
-      drawBookmarkLayout(item.id)
-      
-    }
+      drawBookmarkLayout(item.id);
+    };
     button.innerText = item.title;
-    bmTree.appendChild(button)
+    bmTree.appendChild(button);
   }
 }
 
 // handle DOM events
 function onBack() {
-  if(isLabelTab) {
-    toggleTab()
-    setCurrentBackButtonLabel()
+  if (isLabelTab) {
+    toggleTab();
+    setCurrentBackButtonLabel();
 
-    return
+    return;
   }
-  if(historyDir.length === 0) return;
+  if (historyDir.length === 0) return;
 
-  const currentDir = historyDir.pop()
-  setCurrentBackButtonLabel()
-  drawBookmarkLayout(currentDir.parentId)
+  const currentDir = historyDir.pop();
+  setCurrentBackButtonLabel();
+  drawBookmarkLayout(currentDir.parentId);
 }
 
 function onMoveToLabel() {
-  toggleTab()
-  backButton.innerHTML = 'Back'
+  toggleTab();
+  backButton.innerHTML = "Back";
+  chrome.storage.sync.get(["colors"], function (result) {
+    console.log("Value currently is " + typeof result.colors);
+    if (result.colors && result.colors.length > 0) {
+      result.colors.forEach(({l, c}) => {
+        const coloredItem = document.createElement("div");
+        coloredItem.setAttribute("style", `background-color: ${c}`);
+        coloredItem.setAttribute("class", "color-item");
+        coloredItem.innerText = l;
+
+        colorsContainer.appendChild(coloredItem);
+      });
+    }
+  });
 }
 
 // utils
 function drawBookmarkLayout(currentItemID) {
-  chrome.bookmarks.getChildren(currentItemID, function (result){
-    const folders = result.filter(i => !i.url)
-    const urls = result.filter(i => i.url)
+  chrome.bookmarks.getChildren(currentItemID, function (result) {
+    const folders = result.filter((i) => !i.url);
+    const urls = result.filter((i) => i.url);
 
-    createBookmarkTree(folders)
-    createBookmarkItem(urls)
-  })
+    createBookmarkTree(folders);
+    createBookmarkItem(urls);
+  });
 }
 
 function setCurrentBackButtonLabel() {
   const { title } = historyDir[historyDir.length - 1] || {};
-  backButton.innerText = title ? title : 'Root'
+  backButton.innerText = title ? title : "Root";
 }
 
 function toggleTab() {
-  labelContainer.classList.toggle('hidden')
-  bookmarkContainer.classList.toggle('hidden')
-  createLabelButton.classList.toggle('hidden')
-  isLabelTab = !isLabelTab
+  labelContainer.classList.toggle("hidden");
+  bookmarkContainer.classList.toggle("hidden");
+  createLabelButton.classList.toggle("hidden");
+  isLabelTab = !isLabelTab;
 }
 
 function createBookmarkItem(items) {
-  itemList.textContent = ''
+  itemList.textContent = "";
 
-  for(let item of items) {
-    const wrapper = document.createElement('div');
-    const anchor = document.createElement('a');
-    const favIcon = document.createElement('img');
+  for (let item of items) {
+    const wrapper = document.createElement("div");
+    const anchor = document.createElement("a");
+    const favIcon = document.createElement("img");
 
-    anchor.href = item.url
-    anchor.target = '_blank'
-    anchor.setAttribute('class', 'item-url')
-    anchor.innerText = item.title
+    anchor.href = item.url;
+    anchor.target = "_blank";
+    anchor.setAttribute("class", "item-url");
+    anchor.innerText = item.title;
 
-    wrapper.setAttribute('class', 'items-wrapper')
-    favIcon.src= `http://www.google.com/s2/favicons?domain=${item.url}`
+    wrapper.setAttribute("class", "items-wrapper");
+    favIcon.src = `http://www.google.com/s2/favicons?domain=${item.url}`;
 
-    wrapper.appendChild(favIcon)
-    wrapper.appendChild(anchor)
-    itemList.appendChild(wrapper)
+    wrapper.appendChild(favIcon);
+    wrapper.appendChild(anchor);
+    itemList.appendChild(wrapper);
   }
 }
